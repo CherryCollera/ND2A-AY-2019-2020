2a6383bf2bbada98739a19995425564b5ec04076
dilrajbrar <60209820+dilrajbrar@users.noreply.github.com>
2020-01-24 20:42:39 +0800

    Add files via upload

---
+++ labacitivity_BRAR/Sample1_Hello World/Sample1_HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+/*18-03470
+ * Dilraj Singh Brar
+ * January 24, 2020
+ * This program will display the phrase "Hello World"
+ */
+
+namespace labacitivity_BRAR
+{
+    class Sample1_HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    
+    }
+}
