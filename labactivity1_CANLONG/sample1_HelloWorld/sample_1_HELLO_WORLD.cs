69a8bfcdc591c57a0d2d0e1dfcd24c76d3a9dbd2
CatherineCanlong <60209882+CatherineCanlong@users.noreply.github.com>
2020-01-24 19:55:32 +0800

    Add files via upload

---
+++ labactivity1_CANLONG/sample1_HelloWorld/sample_1_HELLO_WORLD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 18-01160
+  Catherine Ann O. Canlong
+  ND2A
+  January 24, 2020
+  This program will display the phrase "Hello World"
+ */
+
+namespace labactivity1_CANLONG
+{
+    class sample_1_HELLO_WORLD
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ labactivity1_CANLONG/sample2_MyProfile/sample2_MyProfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 18-01160
+  Catherine Ann O. Canlong
+  ND2A
+  January 24, 2020
+  This program will display the phrase "My Profile"
+ */
+
+namespace sample2_MyProfile
+{
+    class sample2_MyProfile
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Name\t \t: Catherine Ann O. Canlong \n" );
+            System.Console.WriteLine("Date of Birth \t: December 07,1999 \n" );
+            System.Console.WriteLine("Course\t\t: BS Computer Science \n");
+            System.Console.WriteLine("Year\t\t: II \n");
+            System.Console.WriteLine("Section\t\t: ND2A \n");
+            System.Console.ReadKey();
+        }
+    }
+}
