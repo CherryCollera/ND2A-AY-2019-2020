d399fd89096998c44e32ab28e1aa3429da745e87
LovelyJaneCarivellAdriano <60209862+LovelyJaneCarivellAdriano@users.noreply.github.com>
2020-02-01 03:50:02 +0400

    Add files via upload

---
+++ LabActivity2_Adriano/BasicOperations/BasicOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOperations
+{
+    class BasicOperations
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("Enter the first number:\t\t");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number:\t");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum = {0} ", num1 + num2);
+            Console.WriteLine("Difference = {0} ", num1 - num2);
+            Console.WriteLine("Product = {0} ", num1 * num2);
+            Console.WriteLine("Quotient = {0} ", num1 / num2);
+            Console.WriteLine("Remainder = {0} ", num1 % num2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LabActivity2_Adriano/DeclaringConstants/DeclaringConstants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeclaringConstants
+{
+    class DeclaringConstants
+    {
+        static void Main(string[] args)
+        {
+            Double radius;
+            const Double pi = 3.14159;
+            Console.Write("Enter Radius:\t ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            Console.Write("radius: {0:0.0000} ", radius);
+            Console.Write("Area= {0:0.0000} ", pi * radius * radius);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
