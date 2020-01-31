ccf1bbf9761870ba9bf9ede3564abffa81299abc
PaulaJanelle <60209859+PaulaJanelle@users.noreply.github.com>
2020-02-01 07:36:39 +0800

    Add files via upload

---
+++ LabActivity2_Valerio/ComputeAverage/ComputeAverage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeAverage
+{
+    class ComputeAverage
+    {
+        static void Main(string[] args)
+        {
+            Double num1, num2, num3, num4, num5;
+            Console.WriteLine("Enter 5 grades:");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            num2 = Convert.ToDouble(Console.ReadLine());
+            num3 = Convert.ToDouble(Console.ReadLine());
+            num4 = Convert.ToDouble(Console.ReadLine());
+            num5 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The Average is {0:0.000} ", (num1 + num2 + num3 + num4 + num5) / 5);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LabActivity2_Valerio/IfElse/IfElse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse
+{
+    class IfElse
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("Enter first number:\t");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number:\t");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            if (num1 > num2)
+                Console.WriteLine(num1 + " is greater than " + num2);
+            else if (num2 > num1)
+                Console.WriteLine(num2 + " is greater than " + num1);
+            else
+                Console.WriteLine(num1 + " is equal to " + num2);
+            Console.ReadKey();
+        }        
+    }
+}
