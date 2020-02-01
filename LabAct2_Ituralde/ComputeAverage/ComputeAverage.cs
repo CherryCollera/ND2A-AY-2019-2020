dd7e94811b093c37d56d7c8de7c6942e200c56e5
Henryituralde <60210020+Henryituralde@users.noreply.github.com>
2020-02-01 10:51:37 +0800

    Add files via upload

---
+++ LabAct2_Ituralde/ComputeAverage/ComputeAverage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ComputeAverage
+{
+    class ComputeAverage
+    {
+        static void Main()
+        {
+            double num1,num2,num3,num4,num5;
+            Console.Write("Enter 5 grades:   \n");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            num2 = Convert.ToDouble(Console.ReadLine());
+            num3 = Convert.ToDouble(Console.ReadLine());
+            num4 = Convert.ToDouble(Console.ReadLine());
+            num5 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("The average is : {0:0.000}.", (num1 + num2 + num3 + num4 + num5) / 5);
+            Console.ReadKey();
+
+        }
+    }
+}
