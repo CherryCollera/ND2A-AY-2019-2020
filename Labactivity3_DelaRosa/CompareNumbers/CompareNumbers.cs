a9ae627774ccc2c026b74ddefd3056e4150b6ecf
JustineDelaRosa <60209838+JustineDelaRosa@users.noreply.github.com>
2020-02-17 19:14:26 +0800

    Lab3

---
+++ Labactivity3_DelaRosa/CompareNumbers/CompareNumbers.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareNumbers
+{
+    class CompareNumbers
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+
+            Console.Write("Enter First number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Third number: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine(num1 + "  is Greater Than " + num2 + " and " + num3);
+
+                if (num2 < num3)
+                {
+                    Console.WriteLine(num2 + " Is Less than " + num1);
+                    Console.WriteLine(num3 + " is less than " + num1);
+                }
+                else if (num2 > num3)
+                {
+                    Console.WriteLine(num3 + " Is Less than " + num1);
+                    Console.WriteLine(num2 + " Is Less than " + num1);
+                }
+                else
+                {
+                    Console.WriteLine("{0} and {1} Are Equal", num2, num3);
+                }
+            }
+            if (num1 < num2 && num1 < num3)
+            {
+                Console.WriteLine(num1 + " Is Less than" + num2 + " and " + num3);
+                if (num2 == num3)
+                {
+                    Console.WriteLine("{0} and {1} Are Equal", num2, num3);
+                }
+            }
+            if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine(num2 + " is Greater Than " + num1 + " and " + num3);
+                if (num1 > num3)
+                {
+                    Console.WriteLine(num1 + " Is Less " + num2);
+                    Console.WriteLine(num3 + " is less than " + num2);
+                }
+                else if (num1 < num3)
+                {
+                    Console.WriteLine(num3 + " Is Less " + num2);
+                    Console.WriteLine(num1 + " Is Less " + num2);
+                }
+                else
+                {
+                    Console.WriteLine("{0} and {1} Are Equal", num1, num3);
+                }
+            }
+            if (num2 < num3 && num2 < num1)
+            {
+                Console.WriteLine(num2 + " Is Less than" + num3 + " and " + num1);
+                if (num3 == num1)
+                {
+                    Console.WriteLine("{0} and {1} Are Equal", num3, num1);
+                }
+            }
+
+            if (num3 > num2 && num3 > num1)
+            {
+                Console.WriteLine(num3 + " Is greater than" + num2 + " and " + num1);
+                if (num2 > num1)
+                { 
+                    Console.WriteLine(num1 + " is less than " + num3);
+                    Console.WriteLine(num2 + " is less than " + num3);
+                 }
+                else if (num2 < num1)
+                {
+                Console.WriteLine(num2 + " Is Less " + num3);
+                Console.WriteLine(num1 + " Is Less " + num3);
+                }
+                else
+                {
+                    Console.WriteLine("{0} and {1} Are Equal", num2, num1);
+
+                }
+            }
+            if (num3 < num1 && num3 < num1)
+            {
+                Console.WriteLine(num3 + " Is Less than" + num2 + " and " + num1);
+                if (num2 == num1)
+                {
+                    Console.WriteLine("{0} and {1} Are Equal", num2, num1);
+                }
+            }
+            if (num1 == num2 && num1 == num3 && num2 == num3)
+            {
+                Console.WriteLine(num1 + "," + num2 + " And " + num3 + " Are Equals ");
+                
+
+            } Console.ReadKey();
+        }
+    }
+}
---
+++ Labactivity3_DelaRosa/GradingSystem/GradingSystem.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace GradingSystem
+{
+    class GradingSystem
+    {
+        static void Main(string[] args)
+        {
+            Double x;
+            String Inco;
+
+            Console.Write("Enter Your Final Grades:");
+            Inco = Console.ReadLine();
+
+            try
+            {
+                if (Inco.Equals("INC") || Inco.Equals("inc"))
+                {
+                    Console.WriteLine("Grade Equivalent     :   INCOMPLETE");
+                    Console.WriteLine("Remarks\t\t     :   INCOMPLETE");
+                }
+                else
+                {
+                    x = Convert.ToDouble(Inco);
+                    if (x >= 97.50 && x <= 100.00)
+                    {
+                        Console.WriteLine("Grade Equivalent to       : 1.0");
+                        Console.WriteLine("Remarks      : Excellent");
+                        Console.ReadKey();
+                    }
+
+
+                    if (x >= 94.50 && x <= 97.49)
+                    {
+                        Console.WriteLine("Grade Equivalent to: 1.25");
+                        Console.WriteLine("Remarks: Excellent");
+                        Console.ReadKey();
+                    }
+
+
+                    if (x >= 91.50 && x <= 94)
+                    {
+                        Console.WriteLine("Grade Equivalent to: 1.50");
+                        Console.WriteLine("Remarks: Very Good");
+                        Console.ReadKey();
+                    }
+
+
+                    if (x >= 88.50 && x <= 91)
+                    {
+                        Console.WriteLine("Grade Equivalent to: 1.75");
+                        Console.WriteLine("Remarks: Very Good");
+                        Console.ReadKey();
+                    }
+
+
+                    if (x >= 85.50 && x <= 88)
+                    {
+                        Console.WriteLine("Grade Equivalent to: 2.00");
+                        Console.WriteLine("Remarks:Good");
+                        Console.ReadKey();
+                    }
+
+
+                    if (x >= 79.50 && x <= 85)
+                    {
+                        Console.WriteLine("Grade Equivalent to: 2.50");
+                        Console.WriteLine("Remarks: Fair");
+                        Console.ReadKey();
+                    }
+
+
+                    if (x >= 76.50 && x <= 79)
+                    {
+                        Console.WriteLine("Grade Equivalent to: 2.75");
+                        Console.WriteLine("Remarks: Fair");
+                        Console.ReadKey();
+                    }
+
+
+                    if (x >= 74.50 && x <= 76)
+                    {
+                        Console.WriteLine("Grade Equivalent to: 3.0");
+                        Console.WriteLine("Remarks: Passed");
+                        Console.ReadKey();
+                    }
+
+
+                    if (x >= 72.00 && x <= 74.49)
+                    {
+                        Console.WriteLine("Grade Equivalent to: 4.00");
+                        Console.WriteLine("Remarks: Conditional (Mt only)");
+                        Console.ReadKey();
+                    }
+
+
+                    if (x >= 60 && x <= 70)
+                    {
+                        Console.WriteLine("Grade Equivalent to: 5.00");
+                        Console.WriteLine("Remarks: failed");
+                        Console.ReadKey();
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
