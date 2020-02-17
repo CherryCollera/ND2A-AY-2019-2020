721948ea1439c3949dd73023e441138d2c403188
Julian Kyrene S. Capuli <60210004+kyrenecapuli@users.noreply.github.com>
2020-02-17 20:09:24 +0800

    LabActivity3Capuli

---
+++ LabActivity3_CAPULI/CompareNumbers/CompareNumbers.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareNumbers
+{
+    class CompareNumbers
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.Write("Enter 1st number ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2nd number ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 3rd number ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("{0} is greater than {1} and {2}", num1, num2, num3);
+                if (num2 < num3)
+                {
+                    Console.WriteLine("{0} is less than {1}", num2, num1);
+                    Console.WriteLine("{0} is less than {1}", num3, num1);
+                }
+                else if (num3 < num2)
+                {
+                    Console.WriteLine("{0} is less than {1}", num3, num1);
+                    Console.WriteLine("{0} is less than {1}", num2, num1);
+                }
+
+                else
+                {
+                    Console.WriteLine("{0} and {1} are equal", num2, num3);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} , {1} and {2} are equal", num1, num2, num3);
+            }
+
+            if (num2 > num1 && num2 > num3) //For 2nd greatest number
+            {
+                Console.WriteLine("{0} is greater than {1} and {2}", num2, num1, num3);
+
+                if (num1 < num3)
+                {
+                    Console.WriteLine("{0} is less than {1}", num1, num2);
+                    Console.WriteLine("{0} is less than {1}", num3, num2);
+                }
+                else if (num3 < num1)
+                {
+                    Console.WriteLine("{0} is less than {1}", num3, num2);
+                    Console.WriteLine("{0} is less than {1}", num1, num2);
+                }
+                else
+                {
+                    Console.WriteLine("{0} and {1} are equal", num1, num3);
+                }
+            }
+            if (num3 > num1 && num3 > num2) //For 3rd greatest number
+            {
+                Console.WriteLine("{0} is greater than {1} and {2}", num3, num1, num2);
+
+                if (num1 < num2)
+                {
+                    Console.WriteLine("{0} is less than {1}", num1, num3);
+                    Console.WriteLine("{0} is less than {1}", num2, num3);
+                }
+                else if (num2 < num1)
+                {
+                    Console.WriteLine("{0} is less than {1}", num2, num3);
+                    Console.WriteLine("{0} is less than {1}", num1, num3);
+                }
+                else
+                {
+                    Console.WriteLine("{0} and {1} are equal", num1, num2);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabActivity3_CAPULI/GradingSystem/GradingSystem.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingSystem
+{
+    class GradingSystem
+    {
+        static void Main(string[] args)
+        {
+            double grade;
+            string incomplete;
+
+            Console.Write("Enter your final grade : ");
+            incomplete = Console.ReadLine();
+            try {
+                if (incomplete.Equals("INC") || incomplete.Equals("inc"))
+                {
+                    Console.WriteLine("Grade Equivalent\t: INC");
+                    Console.Write("\nRemarks\t\t: Incomplete");
+                }
+
+                else
+                {
+                    grade = Convert.ToDouble(incomplete);
+                    if (grade >= 97.60 && grade <= 100.0)
+                    {
+
+                        Console.Write("Grade Equivalent\t: 1.00");
+                        Console.Write("\nRemarks\t\t: Excellent");
+                    }
+                    else if (grade >= 94.60 && grade <= 97.59)
+                    {
+                        Console.Write("Grade Equivalent\t: 1.25");
+                        Console.Write("\nRemarks\t\t: Excellent");
+                    }
+                    else if (grade >= 91.60 && grade <= 94.59)
+                    {
+                        Console.Write("Grade Equivalent\t: 1.50");
+                        Console.Write("\nRemarks\t\t: Very Good");
+                    }
+                    else if (grade >= 88.60 && grade <= 91.59)
+                    {
+                        Console.Write("Grade Equivalent\t: 1.75");
+                        Console.Write("\nRemarks\t\t: Very Good");
+                    }
+                    else if (grade >= 85.60 && grade <= 88.59)
+                    {
+                        Console.Write("Grade Equivalent\t : 2.00");
+                        Console.Write("\nRemarks\t\t: Good");
+                    }
+                    else if (grade >= 82.60 && grade <= 85.59)
+                    {
+                        Console.Write("Grade Equivalent\t: 2.25");
+                        Console.Write("\nRemarks\t\t: Good");
+                    }
+                    else if (grade >= 79.60 && grade <= 82.59)
+                    {
+                        Console.Write("Grade Equivalent\t: 2.50");
+                        Console.Write("\nRemarks\t\t: Fair");
+                    }
+                    else if (grade >= 76.60 && grade <= 79.59)
+                    {
+                        Console.Write("Grade Equivalent\t : 2.75");
+                        Console.Write("\nRemarks\t\t: Passed");
+                    }
+                    else if (grade >= 74.60 && grade <= 76.59)
+                    {
+                        Console.Write("Grade Equivalent\t : 3.00");
+                        Console.Write("\nRemarks\t\t: Passed");
+                    }
+                    else if (grade >= 71.60 && grade <= 74.59)
+                    {
+                        Console.Write("Grade Equivalent\t: 4.00");
+                        Console.Write("\nRemarks\t\t: Conditional(MT only)");
+                    }
+                    else if ((grade >= 59.60) && (grade <= 71.59))
+                    {
+                        Console.Write("Grade Equivalent\t: 5.00");
+                        Console.Write("\nRemarks\t\t: Failed");
+                    }
+                    else //For input of grades less than 60.
+                    {
+                        Console.Write("Grade Equivalent\t: 5.00");
+                        Console.Write("\nRemarks\t\t: Failed");
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+
+            }
+
+    }
+}
