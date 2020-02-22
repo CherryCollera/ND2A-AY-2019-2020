32ed658574a4fff2405c363ba4c25ab8bf089f43
CatherineCanlong <60209882+CatherineCanlong@users.noreply.github.com>
2020-02-22 16:19:35 +0800

    Lab_Activity4_CANLONG

---
+++ Lab_Activity4_CANLONG/ClassExample1_Canlong/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassExample1_Canlong
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Print p = new Print();
+            p.PrintDetails();
+            Console.ReadLine();
+        }
+    }
+}
