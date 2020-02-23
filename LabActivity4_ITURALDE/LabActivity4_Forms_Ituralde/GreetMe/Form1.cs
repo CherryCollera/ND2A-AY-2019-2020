079e121a209c479a674989bbede9bc7f02a6ea63
Julian Kyrene S. Capuli <60210004+kyrenecapuli@users.noreply.github.com>
2020-02-23 11:39:50 +0800

    LabActivity4Ituralde

---
+++ LabActivity4_ITURALDE/LabActivity4_Forms_Ituralde/GreetMe/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreetMe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bttn_GetMessage_Click(object sender, EventArgs e)
+        {
+            HappyBirthday hb = new HappyBirthday();
+            MessageBox.Show(hb.getmessage("Happy Birthday!!"));
+        }
+
+        private void bttn_Hide_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
---
+++ LabActivity4_ITURALDE/LabActivity4_Ituralde/ClassExample1.2_Ituralde/Print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample1._2_Ituralde
+{
+    class Print
+    {
+        public void PrintDetails()
+        {
+
+            //Printing value of name variable
+            System.Console.Write("Hello " + Accept.firstname + " " + Accept.lastname + "!!!\nYou have created classes in OOP");
+            /**MyProfile mp = new MyProfile();
+            mp.DisplayProfile();**/
+
+        }
+    }
+}
