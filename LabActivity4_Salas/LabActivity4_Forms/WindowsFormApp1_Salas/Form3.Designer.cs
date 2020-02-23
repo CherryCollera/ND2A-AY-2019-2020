52c982a81a21c01ce89a6e758ab7414fe4cc71dc
LiraSalas <60209930+LiraSalas@users.noreply.github.com>
2020-02-23 08:45:07 +0800

    Summary

---
+++ LabActivity4_Salas/LabActivity4_Forms/WindowsFormApp1_Salas/Form3.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormApp1_Salas
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fname = new System.Windows.Forms.Label();
+            this.lname = new System.Windows.Forms.Label();
+            this.myprofile = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Hide = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // fname
+            // 
+            this.fname.AutoSize = true;
+            this.fname.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fname.Location = new System.Drawing.Point(14, 66);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(57, 13);
+            this.fname.TabIndex = 0;
+            this.fname.Text = "FirstName:";
+            this.fname.Click += new System.EventHandler(this.fname_Click);
+            // 
+            // lname
+            // 
+            this.lname.AutoSize = true;
+            this.lname.Location = new System.Drawing.Point(14, 91);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(56, 13);
+            this.lname.TabIndex = 1;
+            this.lname.Text = "Lastname:";
+            // 
+            // myprofile
+            // 
+            this.myprofile.AutoSize = true;
+            this.myprofile.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myprofile.Location = new System.Drawing.Point(103, 24);
+            this.myprofile.Name = "myprofile";
+            this.myprofile.Size = new System.Drawing.Size(113, 23);
+            this.myprofile.TabIndex = 2;
+            this.myprofile.Text = "My Profile";
+            this.myprofile.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.Font = new System.Drawing.Font("Kristen ITC", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(12, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Get My Profile";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Hide
+            // 
+            this.Hide.BackColor = System.Drawing.Color.LightCoral;
+            this.Hide.Font = new System.Drawing.Font("Kristen ITC", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hide.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Hide.Location = new System.Drawing.Point(141, 132);
+            this.Hide.Name = "Hide";
+            this.Hide.Size = new System.Drawing.Size(75, 35);
+            this.Hide.TabIndex = 4;
+            this.Hide.Text = "Hide";
+            this.Hide.UseVisualStyleBackColor = false;
+            this.Hide.Click += new System.EventHandler(this.Hide_Click);
+            // 
+            // Back
+            // 
+            this.Back.BackColor = System.Drawing.Color.LightCoral;
+            this.Back.Font = new System.Drawing.Font("Kristen ITC", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Back.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Back.Location = new System.Drawing.Point(222, 132);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(75, 35);
+            this.Back.TabIndex = 5;
+            this.Back.Text = "Back";
+            this.Back.UseVisualStyleBackColor = false;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 196);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.Hide);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.myprofile);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.fname);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fname;
+        private System.Windows.Forms.Label lname;
+        private System.Windows.Forms.Label myprofile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Hide;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
