e3a78a76866e9604a77d576cefe84ee4690041cd
JuvyCaguimbal <60209907+JuvyCaguimbal@users.noreply.github.com>
2020-02-25 11:20:03 +0800

    Lab4Caguimbal

---
+++ LabACtivity4_Caguimbal/LabAct4_Caguimbal/Class2_example_Caguimbal_V2/Cars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2_example_Caguimbal_V2
+{
+    class Cars
+    {
+        private string color;
+
+        public Cars(string color)
+        {
+            this.color = color;
+        }
+        public string Describe()
+        {
+            return "this car is " + color;
+        }
+    }
+}
---
+++ LabACtivity4_Caguimbal/LabAct4_Caguimbal_Form/GreetMe/Form4.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreetMe
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Sum_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            num1 = Convert.ToInt32(txtbx_fnum.Text);
+            num2 = Convert.ToInt32(txt_Secnum.Text);
+            txtbx_Ans.Text = (num1 + num2).ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Diff_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            num1 = Convert.ToInt32(txtbx_fnum.Text);
+            num2 = Convert.ToInt32(txt_Secnum.Text);
+            txtbx_Ans.Text = (num1 - num2).ToString();
+        }
+
+        private void btn_Prod_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            num1 = Convert.ToInt32(txtbx_fnum.Text);
+            num2 = Convert.ToInt32(txt_Secnum.Text);
+            txtbx_Ans.Text = (num1 * num2).ToString();
+        }
+
+        private void btn_Quotient_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            num1 = Convert.ToInt32(txtbx_fnum.Text);
+            num2 = Convert.ToInt32(txt_Secnum.Text);
+            txtbx_Ans.Text = (num1 / num2).ToString();
+        }
+
+        private void btn_Mod_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            num1 = Convert.ToInt32(txtbx_fnum.Text);
+            num2 = Convert.ToInt32(txt_Secnum.Text);
+            txtbx_Ans.Text = (num1 % num2).ToString();
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            Form3 frm = new Form3();
+            this.Hide();
+            frm.Show();
+        }
+    }
+}
