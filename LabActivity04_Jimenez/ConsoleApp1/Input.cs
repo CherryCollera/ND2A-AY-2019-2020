532d099cc3bd2485b4a2c96156465f864763cdaa
JerwinJimenez <jerwinjimenez23@gmail.com>
2020-02-25 08:44:40 +0800

    LabAct4

---
+++ LabActivity04_Jimenez/ConsoleApp1/Input.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BasicOperations_Jimenez
+{
+    class Input
+    {
+        public static double num, nums;
+        public double number1()
+        {
+            System.Console.Write("Input first number: ");
+            num = System.Convert.ToDouble(System.Console.ReadLine());
+            return num;
+        }
+        public double number2()
+        {
+            System.Console.Write("Input Second number: ");
+            nums = System.Convert.ToDouble(System.Console.ReadLine());
+            return nums;
+        }
+    }
+}
+
+
---
+++ LabActivity04_Jimenez/ConsoleApp1/Sum.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicOperations_Jimenez
+{
+    class Sum
+    {
+        public double AcceptSum(double a, double b)
+        {
+            double c = a + b;
+            return c;
+        }
+    }
+}
