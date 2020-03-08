fb602e1bd4a9fca63cba6a5a305012f528e36032
jerwinJimenez <jerwinjimenez23@gmail.com>
2020-03-08 21:07:15 +0800

    lab 5

---
+++ LabActivity5_Jumenez/Calculator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMessage_Click(object sender, EventArgs e)
+        {
+
+          var result =  MessageBox.Show("Hello", "My Message", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (result == DialogResult.Yes)
+            {
+                Calculator frn = new Calculator();
+                frn.Show();
+                this.Hide();
+            } else if (result == DialogResult.No)
+            {
+
+            }
+        }
+    }
+}
