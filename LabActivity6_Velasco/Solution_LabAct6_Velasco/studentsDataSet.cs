8ec4333825a4edc6a236e3087d70e9b86c0524fe
JohnVelasco <60209873+JohnVelasco@users.noreply.github.com>
2020-03-14 23:00:10 +0800

    Lab6

---
+++ LabActivity6_Velasco/Solution_LabAct6_Velasco/studentsDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace Solution_LabAct6_Velasco
+{
+
+
+    partial class studentsDataSet
+    {
+    }
+}
+
+namespace Solution_LabAct6_Velasco.studentsDataSetTableAdapters {
+    
+    
+    public partial class tblStudent_InfoTableAdapter {
+    }
+}
